2f60b76925c77db9e400813695cdffb496f06e8e
khcarver1 <carver.kansas@gmail.com>
2024-08-22 17:57:29 -0600

    Updates

---
+++ precache-manifest.dc79fa655326a7b5d95cb2a513929f79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af255d81b963e30a923e6b4135859f48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bae542df836c5dc6db7b",
+    "url": "/static/css/main.fd16b90c.chunk.css"
+  },
+  {
+    "revision": "f0d7d2cfba7dd07d8deb",
+    "url": "/static/js/2.6c07ac43.chunk.js"
+  },
+  {
+    "revision": "f8860fcb8e6b635f587e",
+    "url": "/static/js/3.0a1b519f.chunk.js"
+  },
+  {
+    "revision": "bae542df836c5dc6db7b",
+    "url": "/static/js/main.fa7fa86e.chunk.js"
+  },
+  {
+    "revision": "7d4e33c8764dceed007c",
+    "url": "/static/js/runtime~main.06ac2507.js"
+  }
+]);
