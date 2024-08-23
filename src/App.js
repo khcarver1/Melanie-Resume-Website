ed515d9f4f724ee9ac745b1973681ab7a2dd5eb0
khcarver1 <carver.kansas@gmail.com>
2024-08-22 22:05:02 -0600

    d

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 import Experience from './Experience';
 import React from 'react';
@@ -19,10 +18,10 @@ function App() {
 
         </div>
         <Switch>
-          <Route exact path="/">
+          <Route exact path="/Melanie-Resume-Website">
             <Home />
           </Route>
-          <Route path="/Experience">
+          <Route path="/Melanie-Resume-Website/Experience">
             <Experience />
           </Route>
 
@@ -38,30 +37,3 @@ export default App;
 
 
 
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
->>>>>>> 4712456238051e6371c4db9c0bd0bb6492621cde
